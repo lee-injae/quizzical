c00845c9bbd1a4f4f9e67da14d65e49ac84b9051
Injae Lee <ilee.injae@gmail.com>
2024-11-11 15:20:41 -0500

    further css adjustments

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -7,6 +7,11 @@ export default function Start(props){
 
     return(
         <div className='start-page'>
+            <a href="https://www.youtube.com/watch?v=DnNnzZ6Ta2c" target="_blank">
+                Ajmal - click here
+            </a>
+            <br/>
+
             <h1 className='title'>Quiz App</h1>
             <h3 className='title-sub'>Road to trivia champion: take one quiz a day</h3>
             <Dropdown
